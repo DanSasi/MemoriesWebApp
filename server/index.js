9e4512726746d54a8222725d4291c3ba7525415f
DanSasi <dan.sasi60@gmail.com>
2022-12-04 15:45:39 +0200

    Connection to MongoDB

--- server/index.js
+++ server/index.js
@@ -3,3 +3,19 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+const app = experss();
+
+app.use(bodyParser.json({limi:"30mb", extended: true}));
+app.use(bodyParser.urlencoded({limi:"30mb", extended: true}));
+app.use(cors());
+
+/*MONGODB SETUP */
+
+const CONNECTION_URL = 'mongodb+srv://dansasi33:dansasi963@cluster0.kwjkf5a.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(()=> app.listen(PORT, ()=> console.log(`Server Running on port: ${PORT}`)))
+.catch((err)=> console.log(err.message));
+
+
